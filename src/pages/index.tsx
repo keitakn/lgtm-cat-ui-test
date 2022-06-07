571339f8023c6e4e30143b9d3df5231236c9508a
keitakn <keita.koga.work@gmail.com>
2022-06-07 23:11:36 +0900

    :arrow_up: @nekochans/lgtm-cat-ui を v0.5.0 にアップグレード

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,14 @@
-import { Footer } from '@nekochans/lgtm-cat-ui';
+import { LayoutContainer } from '@nekochans/lgtm-cat-ui';
 import React from 'react';
 
 import type { NextPage } from 'next';
 
-const IndexPage: NextPage = () => {
-  const termsUrl = 'https://lgtmeow.com/terms';
-
-  const privacyUrl = 'https://lgtmeow.com/privacy';
-
-  const terms = {
-    linkText: <a href={termsUrl}>利用規約</a>,
-  };
-
-  const privacy = {
-    linkText: <a href={privacyUrl}>Privacy Policy</a>,
-  };
-
-  return <Footer terms={terms} privacy={privacy} />;
-};
+const IndexPage: NextPage = () => (
+  <LayoutContainer useNextLink={true}>
+    <h1>ねこ</h1>
+    <h2>かわいいねこ</h2>
+    <p>ねこたくさん</p>
+  </LayoutContainer>
+);
 
 export default IndexPage;
