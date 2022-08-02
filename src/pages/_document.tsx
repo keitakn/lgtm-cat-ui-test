838dbfc0f5094f50e2ff2e59252460700e6c168d
keitakn <keita.koga.work@gmail.com>
2022-08-02 18:51:00 +0900

    :wrench: フォントが増えたので追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,7 +45,7 @@ export default class CustomDocument extends Document {
       <Html prefix="og: https://ogp.me/ns#">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto&family=Zen+Kaku+Gothic+New&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto&family=Zen+Kaku+Gothic+New&display=swap"
             rel="stylesheet"
           />
         </Head>
