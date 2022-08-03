99c3284e21a1309cdae07af71241b7145e4501d3
keitakn <keita.koga.work@gmail.com>
2022-08-03 23:39:11 +0900

    :recycle: 404ページを正常表示出来るように必要な設定を追加

--- src/templates/ErrorTemplate/ErrorTemplate.tsx
+++ src/templates/ErrorTemplate/ErrorTemplate.tsx
@@ -1,13 +1,20 @@
 import { ErrorTemplate as OrgErrorTemplate } from '@nekochans/lgtm-cat-ui';
 
+import { NotFoundImage } from './NotFoundImage';
+
 import type { FC } from 'react';
 
+// eslint-disable-next-line
+type ErrorType = 404 | 500 | 503;
+
 type Props = {
-  // eslint-disable-next-line
-  type: 404 | 500 | 503;
+  type: ErrorType;
   language: 'ja' | 'en';
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const catImage = (type: ErrorType): JSX.Element => <NotFoundImage />;
+
 export const ErrorTemplate: FC<Props> = ({ type, language }) => (
-  <OrgErrorTemplate type={type} language={language} />
+  <OrgErrorTemplate type={type} language={language} catImage={catImage(type)} />
 );
