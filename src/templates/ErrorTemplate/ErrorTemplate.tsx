68d321ef96dd34caa943186a3385433f6462c0e0
keitakn <keita.koga.work@gmail.com>
2022-08-08 23:40:41 +0900

    :art: 503のパターンを表示出来るように変更

--- src/templates/ErrorTemplate/ErrorTemplate.tsx
+++ src/templates/ErrorTemplate/ErrorTemplate.tsx
@@ -1,7 +1,10 @@
 import { ErrorTemplate as OrgErrorTemplate } from '@nekochans/lgtm-cat-ui';
 
+import { assertNever } from '../../utils/assertNever';
+
 import { InternalServerErrorImage } from './InternalServerErrorImage';
 import { NotFoundImage } from './NotFoundImage';
+import { ServiceUnavailableImage } from './ServiceUnavailableImage';
 
 import type { FC } from 'react';
 
@@ -21,8 +24,11 @@ const catImage = (type: ErrorType): JSX.Element => {
     // eslint-disable-next-line no-magic-numbers
     case 500:
       return <InternalServerErrorImage />;
+    // eslint-disable-next-line no-magic-numbers
+    case 503:
+      return <ServiceUnavailableImage />;
     default:
-      return <InternalServerErrorImage />;
+      return assertNever(type);
   }
 };
 
