ae1ed2b0b6c2f2aabd1b3ef75025a319b62567a5
keitakn <keita.koga.work@gmail.com>
2022-08-09 23:24:09 +0900

    :sparkles: getStaticProps 内でロケールの検証がしたいので、getStaticPropsからlgtmImagesを渡すように変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
+import { LgtmImage } from '@nekochans/lgtm-cat-ui';
+
 import { TopTemplate } from '../templates/TopTemplate/TopTemplate';
+import { imageData } from '../utils/imageData';
+import { extractRandomImages } from '../utils/randomImages';
 
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
 
-const IndexPage: NextPage = () => <TopTemplate />;
+type Props = {
+  lgtmImages: LgtmImage[];
+};
+
+const IndexPage: NextPage<Props> = ({ lgtmImages }) => (
+  <TopTemplate lgtmImages={lgtmImages} />
+);
+
+// eslint-disable-next-line require-await
+export const getStaticProps: GetStaticProps = async () => {
+  const revalidate = 3600;
+
+  const imageLength = 9;
+  const lgtmImages = extractRandomImages(imageData, imageLength);
+
+  return {
+    props: { lgtmImages },
+    revalidate,
+  };
+};
 
 export default IndexPage;
