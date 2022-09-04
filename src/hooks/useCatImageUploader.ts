8c34bd9276d715520db3e06a16f900b8c77d6f01
keitakn <keita.koga.work@gmail.com>
2022-09-04 15:20:17 +0900

    :sparkles: #26 画像アップロードでエラーが発生した際の対応を追加

--- src/hooks/useCatImageUploader.ts
+++ src/hooks/useCatImageUploader.ts
@@ -1,8 +1,47 @@
 import { issueAccessToken } from '../api/fetch/authToken';
 import { uploadCatImage } from '../api/fetch/lgtmImage';
-import { Language } from '../features/language';
-import { AcceptedTypesImageExtension } from '../features/lgtmImage';
+import { UploadCatImageSizeTooLargeError } from '../features/errors/UploadCatImageSizeTooLargeError';
+import { UploadCatImageValidationError } from '../features/errors/UploadCatImageValidationError';
 import { createSuccessResult, isFailureResult } from '../features/result';
+
+import type { Language } from '../features/language';
+import type { AcceptedTypesImageExtension } from '../features/lgtmImage';
+
+const createCatImageSizeTooLargeErrorMessages = (language: Language) =>
+  language === 'en'
+    ? ['Image size is too large. ', 'Please use images under 4MB.']
+    : [
+        '画像サイズが大きすぎます。',
+        'お手数ですが4MB以下の画像を利用して下さい。',
+      ];
+
+const createDisplayErrorMessages = (
+  error: Error,
+  language: Language,
+): string[] => {
+  if (error instanceof UploadCatImageSizeTooLargeError) {
+    return createCatImageSizeTooLargeErrorMessages(language);
+  }
+
+  if (error instanceof UploadCatImageValidationError) {
+    return language === 'en'
+      ? ['Invalid image format.', 'Sorry, please use another image.']
+      : [
+          '画像フォーマットが不正です。',
+          'お手数ですが別の画像を利用して下さい。',
+        ];
+  }
+
+  return language === 'en'
+    ? [
+        'An unexpected Error occurred.',
+        'Sorry, please try again after some time has passed.',
+      ]
+    : [
+        '予期せぬエラーが発生しました。',
+        'お手数ですが、しばらく時間が経ってからお試し下さい。',
+      ];
+};
 
 const createCatImageUploader =
   (language: Language) =>
@@ -16,7 +55,10 @@ const createCatImageUploader =
     });
     if (isFailureResult(uploadCatImageResult)) {
       return createSuccessResult({
-        displayErrorMessages: [`${language} Errorが発生。`],
+        displayErrorMessages: createDisplayErrorMessages(
+          uploadCatImageResult.value,
+          language,
+        ),
       });
     }
 
