a3d54ec48dab650ce28bcfda9ec33ffd24c6470c
keitakn <keita.koga.work@gmail.com>
2022-09-09 21:49:22 +0900

    :recycle: #49 Props名を他のComponentに合わせて修正

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -3,14 +3,15 @@ import NextErrorComponent, { type ErrorProps } from 'next/error';
 
 import type { NextPage } from 'next';
 
+// このComponentは https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/ を参考にして実装した
 const CustomErrorComponent: NextPage<ErrorProps> = (props) => (
   <NextErrorComponent statusCode={props.statusCode} />
 );
 
-CustomErrorComponent.getInitialProps = async (contextData) => {
-  await Sentry.captureUnderscoreErrorException(contextData);
+CustomErrorComponent.getInitialProps = async (context) => {
+  await Sentry.captureUnderscoreErrorException(context);
 
-  return NextErrorComponent.getInitialProps(contextData);
+  return NextErrorComponent.getInitialProps(context);
 };
 
 export default CustomErrorComponent;
