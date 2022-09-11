e0c56a65f6eb1d2ba7fa08ca308fc968ae47df62
keitakn <keita.koga.work@gmail.com>
2022-09-11 23:53:16 +0900

    :sparkles: #48 カスタムイベント送信用の関数を実装

--- src/utils/gtm.ts
+++ src/utils/gtm.ts
@@ -19,3 +19,57 @@ export const googleTagManagerId = (): GoogleTagManagerId => {
 
   return 'GTM-T9VPVTR';
 };
+
+declare global {
+  interface Window {
+    dataLayer: Record<string, unknown>[];
+  }
+}
+
+export const sendClickTopFetchRandomCatButton = (): void => {
+  window.dataLayer.push({
+    event: 'ClickTopFetchRandomCatButton',
+  });
+};
+
+export const sendClickTopFetchNewArrivalCatButton = (): void => {
+  window.dataLayer.push({
+    event: 'ClickTopFetchNewArrivalCatButton',
+  });
+};
+
+export const sendClickLanguageMenuJaButton = (): void => {
+  window.dataLayer.push({
+    event: 'ClickLanguageMenuJaButton',
+  });
+};
+
+export const sendClickLanguageMenuEnButton = (): void => {
+  window.dataLayer.push({
+    event: 'ClickLanguageMenuEnButton',
+  });
+};
+
+export const sendUploadedCatImage = (): void => {
+  window.dataLayer.push({
+    event: 'UploadedCatImage',
+  });
+};
+
+export const sendCopyMarkdownFromTopImages = (): void => {
+  window.dataLayer.push({
+    event: 'CopyMarkdownFromTopImages',
+  });
+};
+
+export const sendCopyMarkdownFromCreatedImage = (): void => {
+  window.dataLayer.push({
+    event: 'CopyMarkdownFromCreatedImage',
+  });
+};
+
+export const sendCopyMarkdownFromCopyButton = (): void => {
+  window.dataLayer.push({
+    event: 'CopyMarkdownFromCopyButton',
+  });
+};
