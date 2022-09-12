688618a56a87b69c03609c63457bea6c6f0bcdc8
keitakn <keita.koga.work@gmail.com>
2022-09-12 16:45:07 +0900

    :fire: 不要な関数や型定義があったので削除

--- src/features/result.ts
+++ src/features/result.ts
@@ -3,14 +3,8 @@ import {
   createFailureResult as orgCreateFailureResult,
   isSuccessResult as orgIsSuccessResult,
   isFailureResult as orgIsFailureResult,
-  type SuccessResult as OrgSuccessResult,
-  FailureResult as OrgFailureResult,
   Result as OrgResult,
 } from '@nekochans/lgtm-cat-ui';
-
-export type SuccessResult<T> = OrgSuccessResult<T>;
-
-export type FailureResult<T> = OrgFailureResult<T>;
 
 export type Result<T, E> = OrgResult<T, E>;
 
