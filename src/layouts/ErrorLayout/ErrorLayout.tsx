67f864b69ab05cd4e7ac4f2f5acb673bdc591e45
keitakn <keita.koga.work@gmail.com>
2022-09-12 14:40:21 +0900

    :recycle: #47 画像の解像度が足りないので元のpngを利用するように変更

--- src/layouts/ErrorLayout/ErrorLayout.tsx
+++ src/layouts/ErrorLayout/ErrorLayout.tsx
@@ -25,25 +25,23 @@ export const ErrorLayout: FC<Props> = ({ title, children, metaTag }) => (
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicons/apple-touch-icon.webp"
+        href="/favicons/apple-touch-icon.png"
       />
       <link
         rel="icon"
-        type="image/webp"
+        type="image/png"
         sizes="32x32"
-        href="/favicons/favicon-32x32.webp"
+        href="/favicons/favicon-32x32.png"
       />
       <link
         rel="icon"
-        type="image/webp"
+        type="image/png"
         sizes="16x16"
-        href="/favicons/favicon-16x16.webp"
+        href="/favicons/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicons/site.webmanifest" />
-      <link rel="shortcut icon" href="/favicons/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
-      <meta name="theme-color" content="#ffffff" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />{' '}
     </Head>
     {children}
   </>
